548aa432ccb77592cad46d7851edc44a360854e4
GustaMantovani <mantovanig000@gmail.com>
2023-10-13 22:31:35 -0300

    script update
    now the new version of php doesnt need user actions, so its possible to move the commands from the install.sh to the dockerfile, wich makes the code more concise

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker compose up -d
-docker exec -it web-server bash -c 'apt install -y php libapache2-mod-php php-mysql && exit' && docker restart web-server
 message="\nDB Server IP:"
 echo -e "$message"
 docker exec -it db-server bash -c 'ifconfig'
